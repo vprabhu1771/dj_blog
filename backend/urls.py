e173b56f40d76e3b8da37dfe06b427ed421c64a3
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2025-04-24 12:04:08 +0530

    Create urls.py
    backend

---
+++ backend/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('post/<slug:slug>/like/', views.like_post, name='like_post'),
+]
