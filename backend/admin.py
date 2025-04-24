8a418dcedd9fb06c55b7c6836c56d0ac4391767b
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2025-04-24 12:14:53 +0530

    Update admin.py
    Register backend models at backend/admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from backend.models import Category, Post, Comment
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comment)
