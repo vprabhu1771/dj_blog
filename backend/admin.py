68c1ebe0abbcb1cf7d84c0f856ce917859d32f95
Prabhu <vprabhu1771@gmail.com>
2025-06-04 20:02:24 +0530

    PostTagInline added to PostAdmin

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from backend.models import Tag, Post
+from backend.models import Tag, Post, PostTag
 
 # Register your models here.
 @admin.register(Tag)
@@ -7,7 +7,19 @@ class TagAdmin(admin.ModelAdmin):
 
     list_display = ('name',)
 
+class PostTagInline(admin.TabularInline):
+    model = PostTag
+    extra = 1  # Number of empty rows shown
+    # autocomplete_fields = ['tag']  # Optional: if you have many tags
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 
-    list_display = ('title', 'content')
+    list_display = ('title', 'content', 'display_tags',)
+
+    inlines = [PostTagInline]  # Adds the tabular inline for PostTag
+
+    def display_tags(self, obj):
+        return ", ".join(tag.name for tag in obj.tags.all())
+
+    display_tags.short_description = 'Tags'
