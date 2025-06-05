5a5eb9948ef5f45256a00be153b04465630ae19c
Prabhu <vprabhu1771@gmail.com>
2025-06-05 12:28:34 +0530

    Proxies for Role-Based Admin Views

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from backend.models import Tag, Post, PostTag, Category, PostCategory
+from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
+
+from backend.forms import CustomUserCreationForm, CustomUserChangeForm
+from backend.models import Tag, Post, PostTag, Category, PostCategory, AuthorUser, MemberUser, AdminUser
 
 
 # Register your models here.
@@ -39,3 +43,46 @@ class PostAdmin(admin.ModelAdmin):
         return ", ".join(category.name for category in obj.categories.all())
 
     display_categories.short_description = 'Categories'
+
+class BaseCustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ('email','gender','image_tag','is_staff','is_active',)
+    list_filter = ('email','is_staff','is_active',)
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    fieldsets = (
+        (None,{'fields':('email','gender','password','groups')}),
+        ('Permissions',{'fields':('is_staff','is_active')}),
+    )
+
+    add_fieldsets = (
+        (None,{
+            'classes': ('wide',),
+            'fields': ('email','gender','password1','password2','is_staff','is_active',)
+        })
+    )
+
+    def image_tag(self,obj):
+        if obj.image:
+            return format_html('<img src="{}"width="50" height="50" />', obj.image.url)
+        return "-"
+
+    image_tag.short_description = 'Image'
+
+# Filter users by group or role (adjust logic if using roles instead of groups)
+@admin.register(AuthorUser)
+class AuthorAdmin(BaseCustomUserAdmin):
+    def get_queryset(self,request):
+        return super().get_queryset(request).filter(groups__name='Author')
+
+@admin.register(MemberUser)
+class MemberAdmin(BaseCustomUserAdmin):
+    def get_queryset(self,request):
+        return super().get_queryset(request).filter(groups__name='Member')
+
+@admin.register(AdminUser)
+class AdminUserAdmin(BaseCustomUserAdmin):
+    def get_queryset(self,request):
+        return super().get_queryset(request).filter(groups__name='Admin')
